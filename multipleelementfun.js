5103b031e63c7299aec4464fbe659ecfe8aa1636
David Prakash <118829009+davidprakash12@users.noreply.github.com>
2022-12-21 16:21:56 +0530

    Add files via upload

---
+++ multipleelementfun.js
@@ -0,0 +1,11 @@
+// Function to return 2 consecutive numbers
+// n is the first number
+function consecutive(n) {
+	var numbers = {}; //Empty object declaration
+
+	numbers.first = n;
+	numbers.second = n+1;
+	numbers.third = n+2;
+
+	return numbers; //Returns numbers object
+}
